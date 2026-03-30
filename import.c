1536e476e5d603d6893f1b443da21e225d60f52c
JakubMadejski <01206793@pw.edu.pl>
2026-03-30 22:33:56 +0200

    import sam skonczony essa ale gejowy projekt

--- import.c
+++ import.c
@@ -1,12 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "import.h"
 
-int file_opener(char filename) {
+Edge* import_from_file(char* filename, int* edge_count) {
     FILE *f;
     f = fopen("filename", "r" );
+    if (!f) {
+        printf("Error opening file!\n");
+        edge_count = 0;
+        return NULL;
+    }
 
-    
+    Edge* edges = malloc(sizeof(Edge) * 100000);  // sprawdzic czy ejst limit dlugosic w 
+    int count = 0;
 
+    char temp_name;
+    int id_A, id_B;
+    double weight;
+    while (fscanf(f, "%s %d %d %lf", temp_name, &id_A, &id_B, &weight) == 4) {
+        edges[count].name = strdup(&temp_name);
+        edges[count].vertex_a = malloc(sizeof(Point));
+        edges[count].vertex_b = malloc(sizeof(Point));
+        edges[count].vertex_a->id = id_A;
+        edges[count].vertex_b->id = id_B;
+        edges[count].weight = weight;
+        count++;
+    }
 
-    fclose()
-    return 0;
+    fclose(f);
+    *edge_count = count;
+    return edges;
 }
